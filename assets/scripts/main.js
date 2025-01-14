f2abdc9a351209e24064133203ba5ba8138a9ebe
Hamdy <tantaacademyt196@gmail.com>
2025-01-14 23:44:43 +0000

    Add: Repos Data

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,11 @@
 let toggleTheme = document.getElementById("toggle-theme");
 document.getElementById("toggle-theme").onclick=()=>{
-    if(document.body.classList.contains("theme") != true){
-        document.body.classList.add("theme");
-        toggleTheme.src="assets/icons/sun.webp";
-    }else{
+    if(document.body.classList.contains("theme")){
         document.body.classList.remove("theme");
         toggleTheme.src="assets/icons/moon.webp";
+    }else{
+        document.body.classList.add("theme");
+        toggleTheme.src="assets/icons/sun.webp";
     }
 }
 
@@ -25,36 +25,52 @@ function getData(){
         message.style.color = "rgb(196, 34, 34)";
     }else{
         fetch(`https://api.github.com/users/${searchInput.value}`)
-        .then(res=>res.json())
+        .then((res)=>res.json())
         .then((userData)=>{
+            message.style.display = "none";
             data = `
                 <div class="user-data">
                     <div class="user-image name">
                         <img src="${userData.avatar_url}" alt="User Image">
                         <h1>${userData.name}</h1>
                     </div>
-                    <div class="user-info">
-                        <div class="location">
+                <div class="user-info">
+                    <div class="location">
                         <span class="title">Location:</span>
                         <span class="description">${userData.location || "Not Founded"}</span>
-                        </div>
-                        <div class="bio">
+                    </div>
+                    <div class="bio">
                             <span class="title">Bio:</span>
                             <p class="description">${userData.bio || "Not Founded"}</p>
-                        </div>
-                        <div class="username">
+                    </div>
+                    <div class="username">
                             <span class="title">Username:</span>
                             <p class="description">${userData.login}</p>
-                        </div>
-                        <a class="show-profile" href="${userData.html_url}" target="_blank" >Github Profile</a>
                     </div>
+                    <a class="show-profile" href="${userData.html_url || "Not Founded"}" target="_blank" >Github Profile</a>
+                </div>
                 </div>
                 <div class="profile-details">
                         <span class="followers">Followers: ${userData.followers}</span>
                         <span class="following">Following: ${userData.following}</span>
                         <span class="repositories">Repositories: ${userData.public_repos}</span>
                 </div>
-            `
+                <div class="repos-data">
+                    <ul id="repos-data">
+                    </ul>
+                </div>
+                `
+                fetch(`https://api.github.com/users/${searchInput.value}/repos`)
+                .then(res=>res.json())
+                .then((reposData)=>{
+                    let reposOutput = "";
+                    for(let i = 0 ; i<reposData.length ; i++ ){
+                        reposOutput += `
+                        <li><span class="page-name">${reposData[i].name}</span><div class="links"><a href="${reposData[i].html_url}" target="_blank"  class="repo-code">Code</a><a href="${reposData[i].homepage}" target="_blank"  class="home-page">ddddd</a></div></li>
+                        `
+                        document.getElementById("repos-data").innerHTML=reposOutput;
+                    }
+                })
             userDetails.innerHTML = data;
         })
     }
